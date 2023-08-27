1ce9523f131f931e603076714f9deaf770900633
DamarisMusic <136343381+DamarisMusic@users.noreply.github.com>
2023-08-27 16:45:04 -0300

    Add files via upload

---
+++ dami.js
@@ -0,0 +1,42 @@
+function multiplicación(a, b)
+{
+    let resultadoMultiplicación = a * b
+    return resultado
+}
+alert(multiplicación(5, 7))
+
+function división(a, b)
+{
+    let resultadoDivisión = a / b
+    return resultado
+}
+alert(división(16, 8))
+
+function suma(a, b)
+{
+    let resultadoSuma = a + b
+    return resultado
+}
+alert(suma(100, 35))
+
+function resta(a, b)
+{
+    let resultadoResta = a - b
+    return resultado
+}
+alert(resta(60, 20))
+
+function potencia(x)
+{
+    let resultadoPotenciacuadrada = x ^ 2 + 5
+    resultadoPotenciacuadrada = x * x + 5
+    return resultado
+}
+alert(potencia(5))
+
+function saludo()
+{
+    let nombre = prompt('Ingresa tu nombre')
+    return ' Bienvenido ' + nombre
+}
+alert(saludo())
