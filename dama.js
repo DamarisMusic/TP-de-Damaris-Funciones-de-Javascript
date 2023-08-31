f6ffbda574784bee54494ef55ab612dc941fdf3f
DamarisMusic <136343381+DamarisMusic@users.noreply.github.com>
2023-08-31 18:20:19 -0300

    Add files via upload

---
+++ dama.js
@@ -0,0 +1,59 @@
+/*TP de JS programación*/
+
+function potenciaCuadrada(x)
+{
+    let resultadopotenciaCuadrada = x * x
+    return resultado
+}
+alert(potenciaCuadrada(25)) /*1)*/
+
+function potenciaCúbica(x)
+{
+    let resultadopotenciaCúbica = x * x * x
+    return resultado
+}
+alert(potenciaCúbica(50)) /*2)*/
+
+function operacióna(x) /*3)*/
+{
+    operacióna = x ^ 2 - x - 10
+    let resultadooperacióna = x * x - x - 10
+    return resultado
+}
+alert(operacióna(15))
+
+function operaciónb(x)
+{
+    let resultadooperaciónb = (x - 3) * 2
+    return resultado
+}
+alert(operaciónb(8))
+
+function operaciónc(x)
+{
+    let resultadooperaciónc = x * x ^ 2 + 20
+    return resultado
+}
+alert(operaciónc(4))
+
+function operaciónd(x)
+{
+    let resultadooperaciónd = x / 2 + 5
+    return resultado
+}
+alert(operaciónd(10))
+
+function operacióne(x)
+{
+    operacióne = x ^ 3 + x ^ 2 + 10
+    let resultadooperacióne = x * x * x + x * x + 10
+    return resultado
+}
+alert(operacióne(6))
+
+function operaciónf(x)
+{
+    let resultadooperaciónf = (x * 10) * 5
+    return resultado
+}
+alert(operaciónf(20))
