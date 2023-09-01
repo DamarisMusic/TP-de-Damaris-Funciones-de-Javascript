120f5efd8cf2155e610fe7b1c753938a29d40aea
alumno <alumno@Instituto.local>
2023-09-01 09:31:06 -0300

    cambios del js de dami
    ajustes

--- dami.js
+++ dami.js
@@ -40,3 +40,17 @@ function saludo()
     return ' Bienvenido ' + nombre
 }
 alert(saludo())
+
+/*Otro ejemplo para llamar a una variable en js es:*/
+/*crearla*/ let cantidad_libros
+/*llamarla*/cantidad_libros = 5 /*con un valor de 5*/
+
+/*a la variable que llamamos podemos ponerle una suma llamandola a la variable en si, ej:*/
+cantidad_libros = cantidad_libros + 5 /*que en total daría 10 porque antes ya se había asignado otro valor de 5*/
+
+/*¿cómo mostrarlo en la consola?*/
+console.log(cantidad_libros)
+alert(cantidad_libros) /*se pone la alerta para que aparezca un mensaje en el documento*/
+
+/*para que muestre un mensaje en el console.log podemos hacer*/
+console.log('La cantidad de libros es:' + cantidad_libros)
